0d5f4b28d9438c09b8431434fac7cc1c7f4ea235
Joy Ngugi <ngugijoy51@gmail.com>
2025-01-15 09:37:01 +0300

    change

--- myproject/settings.py
+++ myproject/settings.py
@@ -18,6 +18,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 import dj_database_url
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
